8caf452bae49e1ef53dfcbf3f6f7c1f505d4c1cb
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-07-16 11:40:56 +0330

    added google fonts and nuxt ui

---
+++ layouts/Default.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+   
+</script>
+
+<template>
+    <div class="container mx-auto max-w-4xl">
+        <main class="my-10">
+            <slot />
+        </main>
+    </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
+    modules: [
+        '@nuxt/ui',
+        ['@nuxtjs/google-fonts', {
+            families: {
+                'Inter': true,
+                download: true,
+                inject: false
+            },
+            display: 'swap'
+        }]
+    ],
+    css: [
+        '~/assets/css/tailwind.css',
+        '~/assets/css/style.css'
+    ],
     postcss: {
         plugins: {
             tailwindcss: {},
             autoprefixer: {},
         },
-    },
-    css: [
-        '~/assets/css/tailwind.css'
-    ]
+    }
 })
