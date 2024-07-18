4b588ed789e99a9e33560131053ab48698d2d172
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-07-18 21:53:56 +0330

    added profile update

--- pages/settings.vue
+++ pages/settings.vue
@@ -6,10 +6,12 @@
         {
             label: "Profile",
             to: "/settings/profile",
+            icon: 'i-heroicons-user',
         },
         {
             label: "Avatar",
             to: "/settings/avatar",
+            icon: 'i-heroicons-photo',
         }
     ];
     
@@ -17,7 +19,7 @@
 
 <template>
     <div class="grid grid-cols-6 gap-8">
-        <UVerticalNavigation :links="links" class="col-span-2" />
+        <UVerticalNavigation :links="links" class="col-span-2" :ui="{base: 'py-2.5 mb-2'}" />
         <div class="col-span-4">
             <NuxtPage />
         </div>
--- pages/settings/profile.vue
+++ pages/settings/profile.vue
@@ -3,34 +3,72 @@
 // impoprts
 
     import { z } from "zod";
+    import { useMutation } from "@tanstack/vue-query";
 
 // state
 
     const { user } = useServices()
+    const { toastSuccess, toastError } = useAppToast()
 
-    const state = ref<{name: string, email: string}>({
+    const userData = ref<{name: string, email: string}>({
         name: user.value.user_metadata?.full_name || '',
         email: user.value.email,
     });
+    const form = ref<HTMLFormElement>()
+
+    const { handleUpdateUser } = useApis()
+
+// queries
+
+    const { isPending: updateIsPending, mutate: updateMutate } = useMutation({
+        mutationFn: (data: any) => handleUpdateUser(data)
+    })
+
+// computed
 
     const schema = z.object({
         name: z.string().min(2).optional(),
         email: z.string().email(),
     });
     
+// methods
+
+    const handleSubmitUpdate = () => {
+        if (form.value.errors.length) return
+
+        const data = {
+            data: {
+                full_name: userData.value.name
+            }
+        }
+
+        if (userData.value.email !== user.value.email) {
+            data.email = userData.value.email
+        }
+
+        updateMutate(data, {
+            onSuccess: () => {
+                toastSuccess({title: 'Profile updated', description: 'Your profile has been updated'})
+            },
+            onError: (err: Error) => {
+                toastError({title: 'Error updating profile', description: err.message})
+            }
+        })
+    }
+
 </script>
 
 <template>
-    <UForm :state="state" :schema="schema">
+    <UForm :state="userData" :schema="schema" ref="form" @submit="handleSubmitUpdate">
         <UFormGroup class="mb-4" label="Full Name" name="name" size="lg">
-            <UInput v-model="state.name" />
+            <UInput v-model="userData.name" />
         </UFormGroup>
 
         <UFormGroup class="mb-4" label="Email" name="email" size="lg" help="You will receive a confirmation email on both the old and the new addresses if you modify the email address">
-            <UInput v-model="state.email" />
+            <UInput v-model="userData.email" />
         </UFormGroup>
 
-        <UButton type="submit" color="black" variant="solid" label="Save" :pending="isPending" />
+        <UButton type="submit" color="black" variant="solid" label="Save" size="md" class="w-[6rem] flex-center" :loading="updateIsPending" />
     </UForm>
 </template>
 
