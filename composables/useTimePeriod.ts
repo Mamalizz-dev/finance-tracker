816b0dfe15bbcb97435c80632ef988522e9df3d7
KhodeMamalizz <mohammad.hosein.ghorbani82@gmail.com>
2024-07-18 10:35:08 +0330

    added useTimePeriod composable

---
+++ composables/useTimePeriod.ts
@@ -0,0 +1,47 @@
+import { startOfYear, endOfYear, sub, startOfDay, endOfDay, startOfMonth, endOfMonth } from 'date-fns'
+
+export const useTimePeriod = (period: Ref) => {
+    const current = computed(() => {
+        switch (period.value) {
+            case 'Yearly':
+                return {
+                    from: startOfYear(new Date()),
+                    to: new Date()
+                }
+            case 'Monthly':
+                return {
+                    from: startOfMonth(new Date()),
+                    to: new Date()
+                }
+            case 'Daily':
+                return {
+                    from: startOfDay(new Date()),
+                    to: new Date()
+                }
+            }
+        }
+    )
+
+    const previous = computed(() => {
+        switch (period.value) {
+            case 'Yearly':
+                return {
+                    from: startOfYear(sub(new Date(), { years: 1 })),
+                    to: endOfYear(sub(new Date(), { years: 1 }))
+                }
+            case 'Monthly':
+                return {
+                    from: startOfMonth(sub(new Date(), { months: 1 })),
+                    to: endOfMonth(sub(new Date(), { months: 1 }))
+                }
+            case 'Daily':
+                return {
+                    from: startOfDay(sub(new Date(), { days: 1 })),
+                    to: endOfDay(sub(new Date(), { days: 1 }))
+                }
+            }
+        }
+    )
+
+  return { current, previous }
+}
